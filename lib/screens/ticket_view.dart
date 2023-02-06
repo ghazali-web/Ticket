af0ed3a4cf6a6905a97c2833911ba9e29bb5c605
ghazali-web <dejahichem96@gmail.com>
2023-02-06 23:28:55 +0100

    2:16:42

--- lib/screens/ticket_view.dart
+++ lib/screens/ticket_view.dart
@@ -1,4 +1,5 @@
-import 'package:book_ticket/thick_container/thickContainer.dart';
+import 'package:gap/gap.dart';
+import 'package:book_ticket/thick_container/thick_container.dart';
 import 'package:book_ticket/utils/app_layout.dart';
 import 'package:book_ticket/utils/app_styles.dart';
 import 'package:flutter/material.dart';
@@ -19,6 +20,7 @@ class TicketView extends StatelessWidget {
         margin: const EdgeInsets.only(left: 16),
         child: Column(
           children: [
+            //showing the orange part of card
             Container(
                 decoration: const BoxDecoration(
                   color: Color(0xFF526799),
@@ -35,15 +37,47 @@ class TicketView extends StatelessWidget {
                         style:
                             Styles.headLineStyle3.copyWith(color: Colors.white),
                       ),
-                      Expanded(
-                          child: SizedBox(
-                              height: 24,
-                              child: Flex(
-                                  direction: Axis.horizontal,
-                                  children: [
-                                    List.generate(5, (index) => Text('-'))
-                                  ]))),
+                      const Spacer(),
                       const ThickContainer(),
+                      Expanded(
+                          child: Stack(
+                        children: [
+                          SizedBox(
+                            height: 24,
+                            child: LayoutBuilder(
+                              builder: (BuildContext context,
+                                  BoxConstraints constraints) {
+                                return Flex(
+                                  direction: Axis.horizontal,
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceBetween,
+                                  children: List.generate(
+                                      (constraints.constrainWidth() / 6)
+                                          .floor(),
+                                      (index) => const SizedBox(
+                                          width: 3,
+                                          height: 1,
+                                          child: DecoratedBox(
+                                            decoration: BoxDecoration(
+                                              color: Colors.white,
+                                            ),
+                                          ))),
+                                );
+                              },
+                            ),
+                          ),
+                          Center(
+                            child: Transform.rotate(
+                                angle: 1.5,
+                                child: const Icon(
+                                  Icons.local_airport_rounded,
+                                  color: Colors.white,
+                                )),
+                          ),
+                        ],
+                      )),
+                      const ThickContainer(),
+                      const Spacer(),
                       Text(
                         'London',
                         style:
@@ -51,7 +85,51 @@ class TicketView extends StatelessWidget {
                       ),
                     ],
                   ),
-                ]))
+                  const Gap(4),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      SizedBox(
+                        width: 120,
+                        child: Text(
+                          'New York',
+                          style: Styles.headLineStyle4
+                              .copyWith(color: Colors.white),
+                        ),
+                      ),
+                      Text(
+                        '8H 30M',
+                        style:
+                            Styles.headLineStyle4.copyWith(color: Colors.white),
+                      ),
+                      SizedBox(
+                        width: 120,
+                        child: Text(
+                          'LDN',
+                          textAlign: TextAlign.end,
+                          style: Styles.headLineStyle3
+                              .copyWith(color: Colors.white),
+                        ),
+                      ),
+                    ],
+                  )
+                ])),
+            //showing the orange part of card
+            Container(
+              color: Styles.orangeColor,
+              child: Row(
+                children: [
+                  Container(
+                    height: 20,
+                    width: 10,
+                    decoration: const BoxDecoration(
+                        borderRadius: BorderRadius.only(
+                            topLeft: Radius.circular(10),
+                            bottomRight: Radius.circular(10))),
+                  )
+                ],
+              ),
+            )
           ],
         ),
       ),
